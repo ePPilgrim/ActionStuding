b166c0a7012c5eb2458e3b4f5b0b11605f48e7a2
PLDD <p.demydiuk@gmail.com>
2021-05-13 08:42:59 +0300

    Some changes

--- src/app.py
+++ src/app.py
@@ -28,7 +28,7 @@ def api():
     :return:
     """
     if request.method == 'GET':
-        return jsonify({'status': 'test'})
+        return jsonify({'status': 'te st'})
     elif request.method == 'POST':
         if validate_post_data(request.json):
             return jsonify({'status': 'OK'})
